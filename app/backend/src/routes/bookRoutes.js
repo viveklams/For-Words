2fa562a60ce2e75de8f12aaaf98958748e999392
viveklams <viveklamgadey@gmail.com>
2025-03-16 18:57:22 +0530

    protect route logice written in before code execution of async routes of bookStore

--- app/backend/src/routes/bookRoutes.js
+++ app/backend/src/routes/bookRoutes.js
@@ -1,13 +1,37 @@
 import express from "express";
+import cloudinary from "../lib/cloudinary.js";
+import protectRoute from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
-router.post("/", async (req,res) => {
-     try{
-        
-     } catch (error) { 
+router.post("/", protectRoute, async (req, res) => {
+  try {
+    const { title, caption, rating, image } = req.body;
 
-     }
-})
+    if (!image || !title || !caption || !rating) {
+      return res.status(400).json({ message: "Please provide all fields" });
+    }
+
+    //upload image to cloudinary
+    const uploadResponse = await cloudinary.uploader.upload(image);
+    const imageUrl = uploadResponse.secure_url;
+    //save to the database
+
+    const newBook = new Book({
+      title,
+      caption,
+      rating,
+      image: imageUrl,
+      // user: req.user._id
+    });
+
+    await newBooksave();
+    res.status(201).json(newBook);
+  } catch (error) {
+    console.log("Error creating book", error);
+    res.status(500).json({ message: error.message });
+  }
+});
 
 export default router;
+rating, (image = req.body);
