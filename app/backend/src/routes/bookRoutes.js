ea3096f073300b657702d7fe0a693266c5f1d7d5
viveklams <viveklamgadey@gmail.com>
2025-03-16 18:16:30 +0530

    cloudinary set up was completed

--- app/backend/src/routes/bookRoutes.js
+++ app/backend/src/routes/bookRoutes.js
@@ -2,4 +2,12 @@ import express from "express";
 
 const router = express.Router();
 
+router.post("/", async (req,res) => {
+     try{
+        
+     } catch (error) { 
+
+     }
+})
+
 export default router;
